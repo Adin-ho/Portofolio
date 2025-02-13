177a03b5037ce85dbedf1b318983f4502de8ecb1
Deoxymp <adinnugroho854@gmail.com>
2025-02-13 16:49:28 +0700

    2

--- src/app/components/AnimeList.jsx
+++ src/app/components/AnimeList.jsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 const AnimeList = ({title, images, id}) => {
     return (
             <Link href={`/${id}`} className="cursor-pointer">
-                <Image src={images} alt="..." width={350} height={350}></Image>
+                <Image src={images} alt="..." width={350} height={350} className="w-full max-h-64 object-cover"></Image>
                 <h1>{title}</h1>
             </Link>
     )
